6300c05d1840e80697471172771493512b548f03
Levan Mzhavanadze <levamzhavanadze@gmail.com>
2024-02-20 11:34:20 +0400

    C

--- 24_DLL/Program.cs
+++ 24_DLL/Program.cs
@@ -1,14 +1,1 @@
-﻿//// See https://aka.ms/new-console-template for more information
-using _24_DLL3;
-
-Console.WriteLine("Hello, World!");
-
-Person person = new Person() { Id = 1, Description = "desc", Name = "John" };
-
-Console.WriteLine(person);
-
-
-CalculatorBasic a = new CalculatorBasic();
-
-
-a.Divide(1,2);
+﻿
