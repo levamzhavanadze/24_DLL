e9784b7c5504b8ed6900737bb7502a39886bbc5d
Levan Mzhavanadze <levamzhavanadze@gmail.com>
2024-02-20 11:16:16 +0400

    Add project files.

---
+++ 24_DLL/Program.cs
@@ -0,0 +1,16 @@
+﻿//// See https://aka.ms/new-console-template for more information
+using _24_DLL3;
+
+Console.WriteLine("Hello, World!");
+
+Person person = new Person() { Id = 1, Description = "desc", Name = "John" };
+
+Console.WriteLine(person);
+
+
+CalculatorBasic a = new CalculatorBasic();
+
+Console.WriteLine(a.Sum(1, 2));
+
+Console.WriteLine(a.Mult(1, 2));
+Console.WriteLine(a.Divide(1, 2));
